bf8b5c0743d4714a7183ff894390d526b38f967d
Brandon Kvarda <brandon.kvarda@databricks.com>
2024-08-29 11:39:57 -0700

    Add tests init

--- tests/test_bricksync.py
+++ tests/test_bricksync.py
@@ -1,9 +1,11 @@
 from bricksync import BrickSync
-from bricksync.config import BrickSyncConfig, ProviderConfig
+from bricksync.config import BrickSyncConfig, ProviderType, ProviderConfig
 import tempfile, pytest
 from unittest.mock import MagicMock, create_autospec, patch
 from bricksync.provider.databricks import DatabricksProvider
 from bricksync.provider.catalog.databricks import DatabricksCatalog
+from bricksync.provider.catalog.snowflake import SnowflakeCatalog
+from bricksync.provider.catalog import Table, CatalogProvider
 from databricks.connect import DatabricksSession
 from databricks.sdk import WorkspaceClient
 from databricks.sdk.credentials_provider import credentials_strategy
@@ -27,6 +29,18 @@ def databricks_catalog(mocker):
     mocker.patch("bricksync.provider.databricks.DatabricksProvider._get_spark_client", return_value=spark)
     dsp = DatabricksProvider(ProviderConfig("databricks", configuration={"cluster_id": "12345"}))
     return DatabricksCatalog(dsp)
+
+@pytest.fixture
+def mock_config():
+    config = MagicMock(spec=BrickSyncConfig)
+    config.providers = [{'databricks': ProviderConfig(ProviderType.DATABRICKS, configuration={})}]
+    return config
+
+@pytest.fixture
+def bricksync(mock_config, databricks_catalog):
+    with patch("bricksync.BrickSync._initialize_provider") as mock_init:
+        mock_init.return_value = databricks_catalog
+        return BrickSync(mock_config)
 
 def test_load_conf():
     with tempfile.NamedTemporaryFile(delete=False) as fp:
@@ -85,6 +99,25 @@ def test_add_bs_provider_lazy_init():
             bs.initialize()
             assert bs.initialized['databricks'] == True
 
+def test_sync(bricksync):
+    source_provider_name = 'databricks'
+    target_provider_name = 'snowflake'
+    source_table_name = 'source_table'
+    target_table_name = 'target_table'
+    
+    source_provider = MagicMock(spec=DatabricksProvider)
+    target_provider = MagicMock(spec=DatabricksProvider)
+    source_table = MagicMock(spec=Table)
+    
+    bricksync.get_provider = MagicMock(side_effect=lambda name: source_provider if name == source_provider_name else target_provider)
+    source_provider.get_table = MagicMock(return_value=source_table)
+    
+    with patch.object(bricksync, '_sync') as mock_sync:
+        bricksync.sync(source_provider_name, source_table_name, target_provider_name, target_table_name)
+        assert mock_sync.called
+        assert mock_sync.call_count == 1
+
+       
 
 
 
